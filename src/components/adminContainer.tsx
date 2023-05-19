e1c993bda5cc9b70ef5dfa1a79b4835b198309bb
m3rashid <m3rashid.hussain@gmail.com>
2023-05-20 01:59:42 +0530

    feat(app): required

--- src/components/adminContainer.tsx
+++ src/components/adminContainer.tsx
@@ -1,17 +1,64 @@
+import { Menu, MenuProps } from 'antd';
+import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
 import React, { PropsWithChildren } from 'react';
-import AdminMenu from 'rca/components/adminMenu';
 
 interface IProps extends PropsWithChildren {}
 
 const AdminContainer: React.FC<IProps> = ({ children }) => {
   const session = useSession();
+  const router = useRouter();
+
+  if (
+    session.status !== 'authenticated' ||
+    // @ts-ignore
+    session.data.user?.type !== 'ADMIN'
+  ) {
+    return null;
+  }
+
+  const subHeaderItems: MenuProps['items'] = [
+    {
+      key: 'gallery',
+      label: 'Gallery',
+      onClick: () => router.push('/admin/gallery'),
+    },
+    {
+      key: 'events',
+      label: 'Events',
+      onClick: () => router.push('/admin/events'),
+    },
+    {
+      key: 'notices',
+      label: 'Notices',
+      onClick: () => router.push('/admin/notices'),
+    },
+    {
+      key: 'test-centers',
+      label: 'Test Centers',
+      onClick: () => router.push('/admin/test-centers'),
+    },
+    {
+      key: 'current-affairs',
+      label: 'Current Affairs',
+      onClick: () => router.push('/admin/current-affairs'),
+    },
+    {
+      key: 'site-settings',
+      label: 'Site Settings',
+      onClick: () => router.push('/admin/site-settings'),
+    },
+  ];
 
   return (
     <>
-      {session.status === 'authenticated' &&
-        // @ts-ignore
-        session.data.user?.type === 'ADMIN' && <AdminMenu />}
+      <Menu
+        theme='light'
+        mode='horizontal'
+        items={subHeaderItems}
+        activeKey={router.pathname.split('/')[2]}
+        style={{ justifyContent: 'center' }}
+      />
 
       {children}
     </>
